3c16bd8a00c601b4da2b224a3f4bb9ca9182aac0
Kevin Wood <krwood90@gmail.com>
2017-07-23 16:15:04 +0000

    added seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+5.times do
+ User.create!(
+  username: Faker::Name.name,
+  email: Faker::Internet.unique.email,
+  password: Faker::Internet.unique.password
+  )
+ end
+ users = User.all
+ 
+ 50.times do
+   wiki = Wiki.create!(
+     user: users.sample,
+     title:  Faker::Name.title,
+     body:   Faker::Lorem.paragraph(2),
+     private: false,
+   )
+   wiki.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+ end
+ wiki = Wiki.all
