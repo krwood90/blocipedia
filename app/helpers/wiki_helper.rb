1cba01586e8528ab16ff53f5601f706749588457
Kevin Wood <krwood90@gmail.com>
2017-07-26 14:45:20 +0000

    added markdown

--- app/helpers/wiki_helper.rb
+++ app/helpers/wiki_helper.rb
@@ -1,2 +1,16 @@
 module WikiHelper
+    def markdown(text)
+       # extensions = [:tables, :fenced_code_blocks, :autolink, :strikethrough, :superscript, :underline, :highlight, :quote, :footnotes]
+       markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
+         autolink: true,
+         tables: true,
+         fenced_code_blocks: true,
+         strikethrough: true,
+         superscript: true,
+         underline: true,
+         highlight: true,
+         quote: true,
+         footnotes: true)
+       markdown.render(text).html_safe
+    end
 end
